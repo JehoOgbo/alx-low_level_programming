2a1ce6e9d7f88545256b78f4826e60bb42bdb807
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-08 06:38:35 -0700

    using puts to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	char puzzle[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(puzzle);
+	return (0);
+}
