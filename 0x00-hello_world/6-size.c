fb409e8b934e45ddd6230b8aaecf3ec17dcbdbe2
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-08 08:35:47 -0700

    fixed bugs and syntax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ *
+ * prints the sizes of various types on yhe computer it is
+ * compiled and run on
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char a = sizeof(char);
-	char b = sizeof(int);
-	char c = sizeof(long int);
-	char d = sizeof(long long int);
-	char e = sizeof(float);
+	int a = sizeof(char);
+	int b = sizeof(int);
+	int c = sizeof(long int);
+	int d = sizeof(long long int);
+	int e = sizeof(float);
 
-	printf("Size of a char: %s". "a");
-	printf("Size of an int: %s"."b");
-	printf("Size of a long int: %s"."c");
-	printf("Size of a long long int: %s"."d");
-	printf("Size of a float: %s"."e");
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %d bytes(s)\n", d);
 
 	return (0);
 }
