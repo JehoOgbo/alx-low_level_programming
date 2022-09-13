daf65928c98544730193809ed8fcc97e029850d1
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-13 05:37:03 -0700

    Add new file 4-isalpha.c and updated main.h
    4-isalpha.c checks if a character is an alphabet
    main.h was updated to contain the new function.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - function
+ *
+ * description: checks for alphabetic character.
+ *
+ * Return: 1 if alphabetic and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
