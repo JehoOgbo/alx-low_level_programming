60de1fa9d05dbfcfa37ce7f5417d1910e9f3f3f4
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-13 09:56:51 -0700

    using if before while in 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,24 +9,21 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n < 98)
 	{
-		_putchar(n + '0');
-		if (n != 98)
+		while (n < 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
+			++n;
 		}
-		++n;
 	}
-	while (n > 98)
+	if (n > 98)
 	{
-		_putchar(n + '0');
-		if (n != 98)
+		while (n > 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
+			--n;
 		}
-		--n;
 	}
+	printf("98\n");
 }
