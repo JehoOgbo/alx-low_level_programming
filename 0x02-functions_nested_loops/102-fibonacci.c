497df037de16676d3642f48dcdcdb2ce31102beb
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-14 03:48:03 -0700

    102-fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print the first 50 fibonacci numbers
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	long int sum, firnum, secnum;
+       	int counter;
+
+	sum = 0; firnum = 0;
+	secnum = 1;
+	counter = 0;
+
+	for (counter = 1; counter < 51; counter++)
+	{
+		sum = firnum + secnum;
+		printf("%ld, ", sum);
+		firnum = secnum;
+		secnum = sum;
+	}
+	sum = firnum + secnum;
+	printf("%ld\n", sum);
+	return (0);
+}
