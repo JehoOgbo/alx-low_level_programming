2f0d4a310af445b0cc55e1763d15ad355f6ed7b2
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-15 13:09:40 -0700

    fixing 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,28 @@ int main(void)
 	num = 1;
 	while (num <= 100)
 	{
-		if ((num % 3 != 0) && (num % 5 != 0))
-			printf("%d ", num);
-		else if ((num % 3 == 0) && (num % 5 != 0))
-			printf("Fizz ");
-		else if ((num % 3 != 0) && (num % 5 == 0))
-			printf("Buzz ");
+		if (num % 3 == 0.&& num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		}
 		else
-			printf("FizzBuzz ");
+		{
+			printf("%i", num);
+		}
+
+		if (num != 100)
+		{
+			putchar(' ');
+		}
+
 		num++;
 	}
 	putchar('\n');
