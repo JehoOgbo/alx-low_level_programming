0f6b326df9f5f023bd20ad5c35f6af9a75d0f2dd
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-19 04:10:28 -0700

    Hopefully done no 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function to reverse a string
+ *
+ * @s: input string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int n;
+	int a;
+	char str[] = "";
+
+	a = 0;
+	n = strlen(s) - 1;
+	while (n >= 0)
+	{
+		str[a] = s[n];
+		--n;
+		++a;
+	}
+	s = str;
+}
