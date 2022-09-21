13d7863e39aa86fc3feb14ada43f0c868e7c107b
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-21 03:39:10 -0700

    checking first three tasks

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings until size limit
+ *
+ * @dest: first string
+ * @src: stringbto be concatenated
+ * @n: operand number
+ *
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++;)
+		dest[len + 1] = src[i];
+	dest[len + i] = '\0';
+
+	return (dest);
+}
