6ebf8639e4a04a634f5dfff78307bf02b4d9d623
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-21 08:51:19 +0000

    baby steps

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme file is in a directory meant for tinkering with pointers, arrays or strings.
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - outputs a single character
+ *
+ * @c: input char
+ *
+ * Return: void
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
