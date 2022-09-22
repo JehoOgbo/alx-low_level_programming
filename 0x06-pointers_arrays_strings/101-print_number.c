46f7d52d9d130c4038ee90e9f9ca40ae3592f697
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-22 08:13:21 +0000

    Worrking on advanced tasks
    Started working on advanced tasks. Successfully coded rot13 encryption and started task to print a number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print-number - function that prints numbers
+ *
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 10)
+		_putchar(n);
+	else if (n > 10 && n < 100)
+}
