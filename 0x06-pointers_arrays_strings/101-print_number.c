ece0d1c4034e1e4a1c0e3fda4b9fc057817f8e6e
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-23 12:23:35 +0000

    Modified program to print numbers not greater than 4 digits

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,38 @@
  */
 void print_number(int n)
 {
-	if (n < 10)
-		_putchar(n);
-	else if (n > 10 && n < 100)
+	int m, d, c, b;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = n * -1;
+	}
+	else
+		m = n;
+
+	d = m / 1000;
+	c = m / 100;
+	b = m / 10;
+
+	if (d > 0)	/* printing number if it is four digit */
+	{
+		_putchar(d + '0');
+		_putchar(((m / 100) % 10) + '0');
+		_putchar(((m / 10) % 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+	else if (c > 0)		/* print number if it is three digit */
+	{
+		_putchar(c + '0');
+		_putchar(((m / 10) % 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+	else if (b > 0)		/* print number if it is two digit */
+	{
+		_putchar(b + '0');
+		_putchar((m % 10) + '0');
+	}
+	else		/* print number if it is only one digit */
+		_putchar(m + '0');
 }
