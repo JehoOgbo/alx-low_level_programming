07e7eaf7c4447ea1b355cda90c47ad1d10d870fe
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-26 14:04:14 +0000

    First attempt at mandatory tasks
    Started easy with not so complex use of functions that find the location of bytes or string. The challenge was the print diagsums function in 8-print_diagsums.c which never seemed to give the right answer no matter the syntax used.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - function to fill the first n bytes of
+ * memory area pointed to by s with constant byte b
+ *
+ * @s: dereferenced pointer to a character
+ * @b: constant byte character
+ * @n: number of bytes of memory to be filled
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This directory shall be used to explore more concepts on the topic of pointers arrays and strings in the c-programming language.
+I shall be exploring pointers to pointers and multidimensional arrays.
