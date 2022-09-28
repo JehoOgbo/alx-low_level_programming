85f52528e6d2ed7534534a15c1bdeb5e41a976d6
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-28 05:09:56 -0700

    Trying today's tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_char - returns 1 for character input
+ * @c: ascii character
+ * Return: 1
+ */
+int is_char(char c)
+{
+	return (1);
+}
+
+/**
+ * _strlen_recursion - calculates the length of a string
+ *
+ * @s: inputted string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (s == '\0')
+		return (0);
+	return (is_char(s[0]) + _strlen_recursion(s + 1));
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This repository will be used to explore the concept of recursions in the c programming language.
