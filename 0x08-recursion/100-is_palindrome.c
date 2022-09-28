e7cf19268f2a8d2b8c86c22c1e9188c28ddd427f
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-09-28 18:49:58 +0000

    Trying to fix issues with coding style and code compilation problems
    Might have to use main.c file if there has to be a next commit.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -21,15 +21,19 @@ int len(char *s)		/* recursive function to get the length of the string */
  *
  * Return: 1 if match, 0 if not match
  */
-int _strcmp(char *s, int i, int j)		/* function doing the actual comparison */
+int _strcmp(char *s, int i, int j)
 {
-	if (s[i] != s[j])		/* checking case if the converging characters don't match */
+	if (s[i] != s[j])
+		/* checking case if the converging characters don't match */
 		return (0);
-	else if (i == j && s[i] == s[j])		/* checking the middle character in odd-length strings */
+	else if (i == j && s[i] == s[j])
+		/* checking the middle character in odd-length strings */
 		return (1);
-	else if (i == j - 1 && s[i] == s[j])		/* checking both middle characters in even-length strings */
+	else if (i == j - 1 && s[i] == s[j])
+		/* checking both middle characters in even-length strings */
 		return (1);
-	else		/* continues running of the function if the middle is not yet reached */
+	else
+		/* continues running of the function if the middle is not yet reached */
 		return (_strcmp(s, i + 1, j - 1));
 }
 
@@ -41,14 +45,18 @@ int _strcmp(char *s, int i, int j)		/* function doing the actual comparison */
  */
 int is_palindrome(char *s)
 {
-	int length, i, j;		/* declaring variables to mark the first and last chars in string */
+	int length, i, j;
+	/* declaring variables to mark the first and last chars in string */
 
-	length = len(s);		/* calling the len() function to get the value of given string */
+	length = len(s);
+	/* calling the len() function to get the value of given string */
 	i = 0;
 	j = length - 1;
 
-	if (length == 0 || length == 1)		/* checks if the string is empty or contains only one character */
+	if (length == 0 || length == 1)
+		/* checks if the string is empty or contains only one character */
 		return (1);
 	else
-		return (_strcmp(s, i, j));		/* sends string into compare funtion: Recursive call */
+		/* sends string into compare funtion: Recursive call */
+		return (_strcmp(s, i, j));
 }
