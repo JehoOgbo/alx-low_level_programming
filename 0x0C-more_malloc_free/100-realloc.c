820cb8f1de2fcf37d23fa18168f9a9dcf1f11ad3
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-10-06 21:13:42 +0000

    seeking points:

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block
+ * using malloc and free
+ *
+ * @ptr: pointer to memory previously allocated
+ * with a call to malloc
+ * @old_size: size in bytes of the allocated space
+ * for ptr
+ * @new_size: new size in bytes of the new memory
+ * block
+ *
+ * Return: void pointer with new memory or NULL
+ * if failure
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+/**	void *new_ptr;
+	unsigned int i;*/
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	/**new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	for (i = 0; i < new_size; ++i)
+	{
+		if (i < old_size)
+			new_ptr[i] = ptr[i];
+		else
+			new_ptr[i] = 0;
+	}
+	free(ptr);
+	return (new_ptr);**/
+	ptr = malloc(new_size);
+
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * only_digit - checks if a string is composed of only numbers
+ *
+ * @str: input string to be checked
+ * Return: 0 or 1
+ */
+int only_digit(char *s)
+{
+	while (*s)
+	{
+		if (isdigit(*s++) == 0)
+			return 0;
+	}
+	return (1);
+}
+/**
+ * main - program to multiply two integers
+ * entered as command line arguments
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success
+ * exit with a status of 98 if failure
+ */
+int main(unsigned int argc, char **argv)
+{
+	char fstr[] = "Error";
+	int i, a = strlen(fstr), b, c;
+	int product, d, e;
+
+	if (argc != 3)
+	{
+		for (i = 0; i < a; ++i)
+			_putchar(fstr[i]);
+		exit(98);
+	}
+	b = only_digit(argv[1]);
+	c = only_digit(argv[2]);
+	if (b && c)
+	{
+		d = atoi(argv[1]);
+		e = atoi(argv[2]);
+	}
+	product = d * e;
+	printf("%d\n", product);
+}
