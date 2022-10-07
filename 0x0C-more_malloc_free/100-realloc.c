f6142061a831026f540f22e8af94c6353b534feb
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-10-07 10:26:47 +0000

    Trying other means and follewing instructions closer

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,12 +20,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-		return (malloc(new_size));
+		ptr = (malloc(new_size));
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	free(ptr);
 	ptr = malloc(new_size);
+
 	return (ptr);
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -19,6 +19,49 @@ int only_digit(char *s)
 	}
 	return (1);
 }
+
+/**
+ * reverse - reverse a string in place
+ *
+ * @s: string to be reversed
+ * Return: void
+ */
+void reverse(char s[])
+{
+	int c, i, j;
+
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
+
+/**
+ * itoa - converts an  integer/number to an alphabet
+ *
+ * @n: number to be converted
+ * @s: string array which alphabets are to be stored
+ * Return: void
+ */
+char *itoa(long int n, char str[])
+{
+	int i, sign;
+
+	if ((sign = n) < 0)	/* record sign */
+		n = -n;		/* make n positive */
+	i = 0;
+	do {	/* generate digits in reverse order */
+		str[i++] = n % 10 + '0';/* get next digit */
+	}while ((n /= 10) > 0);		/* delete it */
+	if (sign < 0)
+		str[i++] = '-';
+	str[i] = '\0';
+	reverse(str);
+	return (str);
+}
+
 /**
  * main - program to multiply two integers
  * entered as command line arguments
@@ -32,22 +75,36 @@ int main(int argc, char **argv)
 {
 	char fstr[] = "Error";
 	int i, a = strlen(fstr), b, c;
-	int product, d, e;
+	long int product, d, e;
+	/* char *ptr = 0, *f;*/
 
 	if (argc != 3)
 	{
 		for (i = 0; i < a; ++i)
 			_putchar(fstr[i]);
+		_putchar('\n');
 		exit(98);
 	}
 	b = only_digit(argv[1]);
 	c = only_digit(argv[2]);
-	if (b && c)
+	if (b == 0 || c == 0)
+	{
+		for (i = 0; i < a; ++i)
+			_putchar(fstr[i]);
+		_putchar('\n');
+		exit(98);
+	}
+	else if (b && c)
 	{
 		d = atoi(argv[1]);
 		e = atoi(argv[2]);
 	}
 	product = d * e;
-	printf("%d\n", product);
+	/* memset(*s, 0, 1); */
+	/* f = itoa(product, ptr); */
+	/* for (i = 0; f[i]; i++)*/
+		/*_putchar(f[i]); */
+	/*_putchar('\n');*/
+	printf("%ld\n", product);
 	return (0);
 }
