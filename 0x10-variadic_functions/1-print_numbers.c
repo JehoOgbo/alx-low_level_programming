386a838e23776fb34bc6ed078a62c147bd0ec551
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-10-13 20:28:13 +0000

    Remove unneccesary if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,19 +15,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list num;
 
-	if (n)
+	va_start(num, n);
+
+	for (i = 0; i < (n - 1); i++)
 	{
-		va_start(num, n);
-
-		for (i = 0; i < (n - 1); i++)
-		{
-			if (separator != NULL)
-				printf("%d%s", va_arg(num, int), separator);
-			else
-				printf("%d", va_arg(num, int));
-		}
-		printf("%d\n", va_arg(num, int));
-
-		va_end(num);
+		if (separator != NULL)
+			printf("%d%s", va_arg(num, int), separator);
+		else
+			printf("%d", va_arg(num, int));
 	}
+	printf("%d\n", va_arg(num, int));
+
+	va_end(num);
 }
