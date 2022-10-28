e39103595dcbb44be72c6c4ab461b0120c63b889
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-10-28 12:35:55 +0000

    Try new files

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,62 +1,27 @@
 #include "main.h"
 
 /**
- * divide - divides a number by another
- * @num: number to be divided
- * @div: divisor
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
  *
- * Return: dividend
- */
-int divide(unsigned long int num, unsigned int div)
-{
-	unsigned long int dividend = 0;
-
-	while (num >= div)
-	{
-		num -= div;
-		dividend++;
-	}
-	return (dividend);
-}
-
-/**
- * find_no_digit - finds the number of digits in a binary number
- * @num: decimal number to be converted and checked
- *
- * Description: this function takes a decimal number and finds how
- * many digits will be in it's binary equivalent.
- *
- * Return: number of digits
- */
-unsigned int find_no_digit(unsigned long int num)
-{
-	unsigned long int count = 0;
-
-	while (num >= 2)
-	{
-		num = divide(num, 2);
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * get_bit - gets the value of a bit at a given index
- * @n: number whose bits are to be checked
- * @index: index of the bit to be checked
- *
- * Return: value of bit at index or -1 if an error occurs
+ * Return: value of the bit.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int check_index;
+	unsigned int i;
 
-	check_index = find_no_digit(n);
-	if (index > check_index)
-		return (-1);
-	if (n & (1 << index))
-		return (1);
-	else
+	if (n == 0 && index < 64)
 		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,55 +1,22 @@
 #include "main.h"
 
 /**
- * divide - divides a number by another
- * @num: number to be divided
- * @div: divisor
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
  *
- * Return: dividend
- */
-int divide(unsigned long int num, unsigned int div)
-{
-	unsigned long int dividend = 0;
-
-	while (num >= div)
-	{
-		num -= div;
-		dividend++;
-	}
-	return (dividend);
-}
-
-/**
- * find_no_digit - finds the number of digits in a binary number
- * @num: decimal number to be converted and checked
- *
- * Description: this function takes a decimal number and finds how
- * many digits will be in it's binary equivalent.
- *
- * Return: number of digits
- */
-unsigned int find_no_digit(unsigned long int num)
-{
-	unsigned int count = 0;
-
-	while (num >= 2)
-	{
-		num = divide(num, 2);
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * set_bit - sets the value of a bit to 1 at a given index
- * @n: number whose bits are to be set
- * @index: index of the bit to be set
- *
- * Return: value of bit at index or -1 if an error occurs
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	*n |= (1 << index);
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+	*n = (*n | m);
+
 	return (1);
 }
