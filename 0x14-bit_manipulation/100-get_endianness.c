d785ac605442bc6140a48483ec1128dff1874296
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-10-28 13:01:43 +0000

    Show endianness of the computer by casting an address as a character

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - shows whether the machine uses big or little endianness
+ * big endianness stores the biggest byte at the least address while little
+ * endianness strores the least byte at the least address
+ *
+ * Return: 0 if big endian or 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char*)&i;
+
+	if (*c)
+		return (1);
+	return (0);
+}
