fd524341e18ae249d8baa1fd1ab417afffce03d1
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-10-28 12:41:31 +0000

    Finished mandatory tasks

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,60 +1,24 @@
 #include "main.h"
 
 /**
- * divide - divides a number by another
- * @num: number to be divided
- * @div: divisor
+ * clear_bit - sets the value of a bit to 0.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
  *
- * Return: dividend
- */
-int divide(unsigned long int num, unsigned int div)
-{
-	unsigned long int dividend = 0;
-
-	while (num >= div)
-	{
-		num -= div;
-		dividend++;
-	}
-	return (dividend);
-}
-
-/**
- * find_no_digit - finds the number of digits in a binary number
- * @num: decimal number to be converted and checked
- *
- * Description: this function takes a decimal number and finds how
- * many digits will be in it's binary equivalent.
- *
- * Return: number of digits
- */
-unsigned int find_no_digit(unsigned long int num)
-{
-	unsigned long int count = 0;
-
-	while (num >= 2)
-	{
-		num = divide(num, 2);
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * clear_bit - clears the value of a bit at a given index
- * @n: number whose bit is to be cleared
- * @index: index of the bit to be cleared
- *
- * Return: value of bit at index or -1 if an error occurs
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int check_index;
+	unsigned int m;
 
-	check_index = find_no_digit(*n);
-	if (index > check_index)
+	if (index > 63)
 		return (-1);
-	*n &= ~(1 << index);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
 	return (1);
 }
