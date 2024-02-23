873e5b4fd2385912cfd83830bcc93c134cfba0a1
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-02-23 21:21:29 +0100

    Algorithm

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,18 +2,30 @@
 
 """ calculate island perimeters"""
 def island_perimeter(grid):
-    """ find the perimeter of an island"""
+    """ find and return the perimeter of an island.
+    The grid represents water by 0 and land by 1.
+
+    Args: grid (list): A list of list of integers representing an island
+    Returns:
+        The perimeter of the island defined in grid.
+    """
     if not grid:
         return
     if grid == []:
         return
-    perimeter = 0
-    for row in grid:
-        for element in row:
-            if element == 1:
-                perimeter+= 2
 
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    perimeter = size * 4 - edges * 2
     return perimeter
-
-if __name__ == '__main__':
-    island_perimeter()
